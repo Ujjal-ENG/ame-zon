c1fe11421ef0c9617af9de37e5cbaab6f0b2e4d3
user <ujjalroy7862@gmail.com>
2023-03-27 01:26:42 +0600

    done the header part

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import Logo from '../assets/images/Logo.svg';
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <header className="bg-blue-800 p-3 text-white">
+            <nav className="flex justify-between items-center mx-20">
+                <img src={Logo} alt="Ame-Jon" />
 
-export default Header
+                <ul className="flex justify-around items-center text-xl gap-6 font-bold">
+                    <li>Order</li>
+                    <li>Order Review</li>
+                    <li>Management Inventory</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
