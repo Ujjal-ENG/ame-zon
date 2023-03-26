497d401f6f8c6826c7fe3cec7fe6cda069ea613c
user <ujjalroy7862@gmail.com>
2023-03-27 02:17:16 +0600

    done the order summary part

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Shop = () => {
-    return <div>Shop</div>;
+    return (
+        <div className="grid grid-cols-5 w-full h-screen">
+            <div className="bg-red-500 col-span-4"></div>
+
+            <div className="bg-orange-300 space-y-3 p-5">
+                <h3 className="text-2xl text-center underline">Order Summary</h3>
+
+                <p>Selected Items: 5</p>
+                <p>Total Price: $112</p>
+                <p>Total Shipping Cost: $112</p>
+                <p>Tax: $112</p>
+                <p className="font-bold text-xl ">Grand Total: $112</p>
+
+                <button type="button" className="px-4 py-2 rounded-md bg-red-500 text-white w-full">
+                    Clear Cart
+                </button>
+                <Link type="button" className="px-4 py-2 rounded-md bg-yellow-500 text-white w-full text-center" to="/order-review">
+                    Review Order
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Shop;
