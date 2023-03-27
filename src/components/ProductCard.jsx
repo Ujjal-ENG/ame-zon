34cc5dfcb4b4ed905592dcefa128ecb26ab5cbd3
user <ujjalroy7862@gmail.com>
2023-03-27 14:58:07 +0600

    order summary functilaity done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,22 +3,25 @@ import { BsCartPlusFill } from 'react-icons/bs';
 
 const ProductCard = (props) => {
     const { name, price, ratings, img, seller } = props.data;
-
+    const handleAddtoCart = (details) => {
+        props.handleAddtoCart(details);
+    };
     return (
         <>
             {/* making the each card */}
             <div className="w-[300px] h-[510px] rounded-md border-2 border-black relative">
                 <img src={img ? img : name} alt={name} className="w-[286px] h-[286px]  mx-auto p-2 rounded-md transition-all hover:scale-110" />
 
-                <h3 className="font-semibold text-2xl p-2">{name.slice(0, 18) + '...'}</h3>
-                <p className="text-xl p-2">Price: ${price}</p>
+                <h3 className="font-semibold text-2xl p-2">{name ? name.slice(0, 18) + '...' : 'Name does not contains'}</h3>
+                <p className="text-xl p-2">Price: ${price ? price : '00'}</p>
 
-                <p className="text-gray-400 p-2">Manufacturer: {seller}</p>
-                <p className="text-gray-400 p-2">Rating: {ratings} star</p>
+                <p className="text-gray-400 p-2">Manufacturer: {seller ? seller : 'Does not exits'}</p>
+                <p className="text-gray-400 p-2">Rating: {ratings ? ratings : '0'} star</p>
 
                 <button
                     className="w-full py-2 bg-orange-300 border-2 border-gray-400 text-xl flex justify-center items-center gap-2 absolute bottom-0 rounded-b-md active:bg-orange-600 transition-all"
-                    type="button">
+                    type="button"
+                    onClick={() => handleAddtoCart(props.data)}>
                     Add to Cart <BsCartPlusFill />
                 </button>
             </div>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import ProductCard from './ProductCard';
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetchData();
@@ -14,25 +15,40 @@ const Shop = () => {
         setProducts(data);
     };
 
+    const handleAddtoCart = (details) => {
+        const newCart = [...cart, details];
+        setCart(newCart);
+    };
+
+    const price = cart.map((el) => el.price);
+    const shipping = cart.map((el) => el.shipping);
+    const newSum = price.reduce((ps, cs) => ps + cs, 0);
+    const shippingCost = shipping.reduce((ps, cs) => ps + cs, 0);
+
+    const total = shippingCost + newSum + 112;
+
+    const handleClearCart = () => {
+        setCart([]);
+    };
     return (
         <div className="grid grid-cols-5 w-full h-screen">
             <div className="col-span-4 grid grid-cols-3 justify-items-center gap-6 p-10">
                 {products &&
                     products.map((el) => {
-                        return <ProductCard key={el.id} data={el} />;
+                        return <ProductCard key={el.id} data={el} handleAddtoCart={handleAddtoCart} />;
                     })}
             </div>
 
             <div className="bg-orange-300 space-y-3 p-5">
                 <h3 className="text-2xl text-center underline">Order Summary</h3>
 
-                <p>Selected Items: 5</p>
-                <p>Total Price: $112</p>
-                <p>Total Shipping Cost: $112</p>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: ${newSum}</p>
+                <p>Total Shipping Cost: ${shippingCost}</p>
                 <p>Tax: $112</p>
-                <p className="font-bold text-xl ">Grand Total: $112</p>
+                <p className="font-bold text-xl ">Grand Total: ${total}</p>
 
-                <button type="button" className="px-4 py-2 rounded-md bg-red-500 text-white w-full">
+                <button type="button" className="px-4 py-2 rounded-md bg-red-500 text-white w-full" onClick={handleClearCart}>
                     Clear Cart
                 </button>
                 <Link type="button" className="px-4 py-2 rounded-md bg-yellow-500 text-white w-full text-center" to="/order-review">
