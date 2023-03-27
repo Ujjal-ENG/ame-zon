88cb7e7344e530d10e3e46e7beebeea095c1c59f
user <ujjalroy7862@gmail.com>
2023-03-27 17:51:11 +0600

    done the home page design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import kops from '../assets/images/kops.jpg';
 const Home = () => {
-    return <div></div>;
+    return (
+        <div className="flex justify-between items-center mx-20 my-5">
+            {/* left side */}
+            <div className="space-y-5 pl-20">
+                <p className="text-yellow-500">Sale up to 70% off</p>
+                <h1 className="text-6xl font-bold">New Collection For Fall</h1>
+                <p className="text-gray-500">Discover all the new arrivals of ready-to-wear collection</p>
+
+                <button type="button" className="px-4 py-2 rounded-md bg-orange-500 text-black ">
+                    Shop Now
+                </button>
+            </div>
+
+            {/* right side */}
+            <img src={kops} alt="" />
+        </div>
+    );
 };
 
 export default Home;
