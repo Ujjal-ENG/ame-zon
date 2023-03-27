2041111cbedd250543ecd29fa6fed5c57ae2cf7c
user <ujjalroy7862@gmail.com>
2023-03-27 14:20:30 +0600

    product image card design and showing is done

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BsCartPlusFill } from 'react-icons/bs';
+
+const ProductCard = (props) => {
+    const { name, price, rating, img, seller } = props.data;
+
+    return (
+        <>
+            {/* making the each card */}
+            <div className="w-[300px] h-[510px] rounded-md border-2 border-black relative">
+                <img src={img} alt={name} className="w-[286px] h-[286px]  mx-auto p-2 rounded-md" />
+
+                <h3 className="font-semibold text-2xl p-2">{name.slice(0, 18) + '...'}</h3>
+                <p className="text-xl p-2">Price: ${price}</p>
+
+                <p className="text-gray-400 p-2">Manufacturer: {seller}</p>
+                <p className="text-gray-400 p-2">Rating: {rating} star</p>
+
+                <button className="w-full py-2 bg-orange-300 border-2 border-gray-400 text-xl flex justify-center items-center gap-2 absolute bottom-0 rounded-b-md" type="button">
+                    Add to Cart <BsCartPlusFill />
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default ProductCard;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import ProductCard from './ProductCard';
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const fetchUrl = await fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json');
+        const data = await fetchUrl.json();
+        setProducts(data);
+    };
+
     return (
         <div className="grid grid-cols-5 w-full h-screen">
-            <div className="bg-red-500 col-span-4"></div>
+            <div className="col-span-4 grid grid-cols-3 justify-items-center gap-6 p-10">
+                {products &&
+                    products.map((el) => {
+                        return <ProductCard key={el.id} data={el} />;
+                    })}
+            </div>
 
             <div className="bg-orange-300 space-y-3 p-5">
                 <h3 className="text-2xl text-center underline">Order Summary</h3>
