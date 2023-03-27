70b6f50f37581f53064648dfd9f8626fc7019db1
user <ujjalroy7862@gmail.com>
2023-03-27 16:41:42 +0600

    design the Order Summary Part

--- src/components/OrderReview.jsx
+++ src/components/OrderReview.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { RiDeleteBin6Fill } from 'react-icons/ri';
 
 const OrderReview = () => {
     const data = useLocation();
-    console.log(data.state);
+    console.log(data);
     return (
         <div className="grid grid-cols-2 justify-center m-20 justify-items-center gap-10">
             {/* show the products details */}
@@ -32,7 +32,22 @@ const OrderReview = () => {
             </div>
 
             {/* show the Order Summary History */}
-            <div></div>
+            <div className="bg-orange-300 w-[400px] text-center mr-48 h-[350px] fixed  right-0  rounded-lg space-y-3 p-5">
+                <h3 className=" text-center underline text-3xl font-bold">Order Summary</h3>
+
+                <p>Selected Items: {data.state.length}</p>
+                <p>Total Price: $10</p>
+                <p>Total Shipping Cost: $11</p>
+                <p>Tax: $112</p>
+                <p className="font-bold text-xl ">Grand Total: $10</p>
+
+                <button type="button" className=" w-full bottom-20 px-4 py-2 rounded-md bg-red-500 text-white " onClick="">
+                    Clear Cart
+                </button>
+                <Link type="button" className="w-full bottom-0 px-4 py-2 rounded-md bg-yellow-500 text-white text-center" to="*">
+                    Proceed Checkout
+                </Link>
+            </div>
         </div>
     );
 };
