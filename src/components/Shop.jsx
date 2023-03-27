40e2bea2e618c1d55ffb06c4b4c8aa8a67767aa2
user <ujjalroy7862@gmail.com>
2023-03-28 01:26:18 +0600

    now everything work properly

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { addToDb, getShoppingCart } from '../utilities/fakedb';
+import { addToDb, deleteShoppingCart, getShoppingCart } from '../utilities/fakedb';
 import ProductCard from './ProductCard';
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -37,24 +37,41 @@ const Shop = () => {
     };
 
     const handleAddtoCart = (details) => {
-        const newCart = [...cart, details];
+        // const newCart = [...cart, details];
+        let newCart = [];
+
+        const exists = cart.find((pd) => pd.id === details.id);
+
+        if (!exists) {
+            details.quantity = 1;
+            newCart = [...cart, details];
+        } else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter((pd) => pd.id !== details.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(details.id);
     };
 
-    const price = cart.map((el) => el.price);
-    const shipping = cart.map((el) => el.shipping);
-    const newSum = price.reduce((ps, cs) => ps + cs, 0);
-    const shippingCost = shipping.reduce((ps, cs) => ps + cs, 0);
-
-    let total = shippingCost + newSum;
-    let tax = Number(((total * 7) / 100).toFixed(2));
-    let grandTotal = total + tax;
+    let tax = 0;
+    let grandTotal = 0;
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    let shippingCost = 0;
+    for (let id of cart) {
+        totalQuantity += id.quantity;
+        totalPrice += id.price * id.quantity;
+        shippingCost += id.shipping;
+    }
+    tax = (totalPrice * 7) / 100;
+    grandTotal = totalPrice + tax + shippingCost;
     const handleClearCart = () => {
         setCart([]);
-        total = 0;
         tax = 0;
         grandTotal = 0;
+        deleteShoppingCart();
     };
     return (
         <div className="grid grid-cols-5 w-full h-screen">
@@ -68,8 +85,8 @@ const Shop = () => {
             <div className="bg-orange-300 space-y-3 p-5 fixed right-10 my-20 rounded-lg">
                 <h3 className="text-2xl text-center underline">Order Summary</h3>
 
-                <p>Selected Items: {cart.length}</p>
-                <p>Total Price: ${newSum}</p>
+                <p>Selected Items: {totalQuantity}</p>
+                <p>Total Price: ${totalPrice}</p>
                 <p>Total Shipping Cost: ${shippingCost}</p>
                 <p>Tax: ${tax}</p>
                 <p className="font-bold text-xl ">Grand Total: ${grandTotal}</p>
