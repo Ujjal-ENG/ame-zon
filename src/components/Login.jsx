b5d2d23300b897ca98cbe311f6577de508824431
user <ujjalroy7862@gmail.com>
2023-03-27 18:20:35 +0600

    now done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,56 +2,54 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <div className="flex w-full justify-center items-center flex-col my-20">
-                <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">SignUp</h2>
+        <div className="flex w-full justify-center items-center flex-col my-20">
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">SignUp</h2>
 
-                <div className="relative mb-4">
-                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">
-                        Name
-                    </label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                </div>
-                <div className="relative mb-4">
-                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                </div>
-                <div className="relative mb-4">
-                    <label htmlFor="message" className="leading-7 text-sm text-gray-600">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                </div>
-                <div className="relative mb-4">
-                    <label htmlFor="message" className="leading-7 text-sm text-gray-600">
-                        Confirm Password
-                    </label>
-                    <input
-                        type="password"
-                        id="confirm-password"
-                        name="confirm-password"
-                        className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                </div>
-                <button className="text-white  bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Sign Up</button>
+            <div className="relative mb-4">
+                <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+                    Name
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
             </div>
+            <div className="relative mb-4">
+                <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+                    Email
+                </label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+            </div>
+            <div className="relative mb-4">
+                <label htmlFor="message" className="leading-7 text-sm text-gray-600">
+                    Password
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+            </div>
+            <div className="relative mb-4">
+                <label htmlFor="message" className="leading-7 text-sm text-gray-600">
+                    Confirm Password
+                </label>
+                <input
+                    type="password"
+                    id="confirm-password"
+                    name="confirm-password"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+            </div>
+            <button className="text-white  bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Sign Up</button>
         </div>
     );
 };
