44c527281ba89b3aad6c17a4f0634ab79fd056b7
user <ujjalroy7862@gmail.com>
2023-03-28 22:52:35 +0600

    fixed the hover issue in the large title area

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,7 +12,9 @@ const ProductCard = (props) => {
             <div className="w-[300px] h-[510px] rounded-md border-2 border-black relative">
                 <img src={img && img} alt={name} className="w-[286px] h-[286px]  mx-auto p-2 rounded-md transition-all hover:scale-110" />
 
-                <h3 className="font-semibold text-2xl whitespace-nowrap overflow-hidden text-ellipsis p-2 w-full hover:text-nowrap">{name ? name : 'Name does not contains'}</h3>
+                <h3 className="font-semibold text-2xl whitespace-nowrap overflow-hidden text-ellipsis p-2 w-full hover:overflow-visible hover:whitespace-normal hover:text-clip hover:text-sm transition-all">
+                    {name ? name : 'Name does not contains'}
+                </h3>
                 <p className="text-xl p-2">Price: ${price ? price : '00'}</p>
 
                 <p className="text-gray-400 p-2">Manufacturer: {seller ? seller : 'Does not exits'}</p>
