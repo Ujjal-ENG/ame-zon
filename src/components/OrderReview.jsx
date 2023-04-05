263afa37c245a449abb73715b1667f03870b86d8
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-05 17:03:58 +0600

    change the deisng in order review section

--- src/components/OrderReview.jsx
+++ src/components/OrderReview.jsx
@@ -14,34 +14,40 @@ const OrderReview = () => {
     console.log(data);
 
     return (
-        <div className="grid grid-cols-2 justify-center m-20 justify-items-center gap-10">
+        <div className="grid grid-cols-4 justify-center m-20">
             {/* show the products details */}
-            <div className="">
+            <div className="col-span-3">
                 {data
                     ? data.map((el) => {
                           const { name, price, img, shipping, quantity } = el;
                           return (
                               <div key={el.id} className="flex w-full  items-center  border-2 border-gray-400 m-5 p-3 rounded-md">
-                                  <img src={img ? img : 'https://picsum.photos/200/300?random'} alt={name} className="w-[150px] h-[150px] my-10" />
+                                  <img src={img ? img : 'https://picsum.photos/200/300?random'} alt={name} className="flex-none w-[250px] h-[250px] my-10" />
 
-                                  <div className="flex flex-col justify-between items-center mr-10">
-                                      <h4 className="text-xl font-semibold">{name.slice(0, 15) + '...'}</h4>
-                                      <p className="text-xl p-2">
-                                          Price: <span className="text-yellow-500">${price ? price : '00'}</span>
-                                      </p>
-                                      <p className="text-xl p-2">
-                                          Selected Qunatity: <span className="text-yellow-500">{quantity}</span>
-                                      </p>
-                                      <p className="text-xl p-2">
-                                          Shipping Charge: <span className="text-yellow-500">${shipping ? shipping : '00'}</span>
-                                      </p>
+                                  <div
+                                      className="grow
+                                  flex  justify-between items-center mr-10">
+                                      <div className="ml-6">
+                                          <h4 className="text-xl font-semibold">{name}</h4>
+                                          <p className="text-xl p-2">
+                                              Price: <span className="text-yellow-500">${price ? price : '00'}</span>
+                                          </p>
+                                          <p className="text-xl p-2">
+                                              Selected Qunatity: <span className="text-yellow-500">{quantity}</span>
+                                          </p>
+                                          <p className="text-xl p-2">
+                                              Shipping Charge: <span className="text-yellow-500">${shipping ? shipping : '00'}</span>
+                                          </p>
+                                      </div>
+
+                                      <div>
+                                          <RiDeleteBin6Fill
+                                              className="flex-none text-7xl text-red-500 rounded-full bg-red-300 p-2 cursor-pointer active:bg-red-600 transition-all"
+                                              type="button"
+                                              onClick={() => handleDelete(el.id)}
+                                          />
+                                      </div>
                                   </div>
-
-                                  <RiDeleteBin6Fill
-                                      className="text-7xl text-red-500 rounded-full bg-red-300 p-2 cursor-pointer active:bg-red-600 transition-all"
-                                      type="button"
-                                      onClick={() => handleDelete(el.id)}
-                                  />
                               </div>
                           );
                       })
@@ -49,7 +55,9 @@ const OrderReview = () => {
             </div>
 
             {/* show the Order Summary History */}
-            <CartDeatis data={data} name="Preview Order" link="*" />
+            <div className="col-span-1">
+                <CartDeatis data={data} name="Preview Order" link="*" />
+            </div>
         </div>
     );
 };
